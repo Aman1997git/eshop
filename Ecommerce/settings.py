f25b238df2e3fc1f231e03e8085fab0ccb19b393
Aman <amandeepkumar01111997@gmail.com>
2021-01-18 18:07:12 +0530

    final commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -125,5 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_assets')
+
+
 MEDIA_URL = '/image/'
 MEDIA_ROOT = BASE_DIR
